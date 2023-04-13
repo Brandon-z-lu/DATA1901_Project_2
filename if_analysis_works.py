faaefab848e624ca216e1ca78a44fd4e8fb5eb8d
Brandon-Lu737 <107624116+Brandon-Lu737@users.noreply.github.com>
2023-04-13 19:24:49 +1000

    Before running the powerful `if_analysis_works.py`

--- if_analysis_works.py
+++ if_analysis_works.py
@@ -1,26 +1,44 @@
 import os
 
-# Function to extract suburb names from a line in suburbs.txt
+
 def extract_suburb_name(line):
     return line.split("/")[-2].strip()
 
-# Read the contents of suburbs.txt
-with open("suburbs.txt", "r") as f:
-    suburbs_content = f.readlines()
 
-# Extract suburb names
-suburb_names = [extract_suburb_name(line) for line in suburbs_content]
+def write_suburbs_to_file(suburb_names):
+    with open("get_suburbs_data/get_suburbs_in.txt", "w") as f:
+        for suburb_name in suburb_names:
+            f.write(f"{suburb_name}\n")
 
-# List all files in csv_cache folder
-csv_files = os.listdir("csv_cache")
 
-# Loop through suburb names and check if any file contains the suburb name
-for suburb_name in suburb_names:
-    found = False
-    for csv_file in csv_files:
-        if suburb_name in csv_file:
-            found = True
-            break
+def update_suburbs_file():
+    os.system("python get_suburbs.py")
+    with open("get_suburbs_data/get_suburbs_out.txt", "r") as f:
+        suburbs_content = f.readlines()
+    with open("suburbs.txt", "w") as f:
+        f.writelines(suburbs_content)
 
-    if not found:
-        print(f"{suburb_name}")
+
+def main():
+    with open("suburbs.txt", "r") as f:
+        suburbs_content = f.readlines()
+    suburb_names = [extract_suburb_name(line) for line in suburbs_content]
+    csv_files = os.listdir("csv_cache")
+    missing_suburbs = []
+    for suburb_name in suburb_names:
+        found = False
+        for csv_file in csv_files:
+            if suburb_name in csv_file:
+                found = True
+                break
+        if not found:
+            missing_suburbs.append(suburb_name)
+    if missing_suburbs:
+        write_suburbs_to_file(missing_suburbs)
+        update_suburbs_file()
+    else:
+        print("All suburbs found in csv_cache")
+
+
+if __name__ == "__main__":
+    main()
