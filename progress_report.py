b5cffeca362f006aa4b37d10bb5e5a4198cb8c73
Brandon-Lu737 <107624116+Brandon-Lu737@users.noreply.github.com>
2023-04-17 21:37:50 +1000

    Randomize the order of All_in_one input

--- progress_report.py
+++ progress_report.py
@@ -2,9 +2,11 @@ import os
 import time
 import schedule
 
+start_time = time.time()
+
 
 def count_files_in_folder(folder_path):
-    return len([f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]) -1
+    return len([f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]) - 1
 
 
 def count_valid_lines(file_path):
@@ -14,13 +16,31 @@ def count_valid_lines(file_path):
 
 
 def progress_report():
+    current_time = time.time()
+    elapsed_time = current_time - start_time
+
     m = count_files_in_folder("csv_cache")
     n = count_valid_lines("main1_INPUT.txt")
     progress = (m / n) * 100
-    num_hashes = int(progress / 2)
-    num_spaces = 50 - num_hashes
+    num_hashes = int(progress)
+    num_spaces = 100 - num_hashes
+
+    if elapsed_time == 0:
+        speed = 0
+    else:
+        speed = m / (elapsed_time / 60)  # files/min
+
+    if speed == 0:
+        eta = 24 * 60
+    else:
+        eta = (n - m) / speed  # minutes
+
+    eta_hours = int(eta // 60)
+    eta_minutes = int(eta % 60)
 
     print(f"{time.strftime('%H:%M:%S')} Progress report:")
+    print(f"Speed: {speed:.2f} files/min")
+    print(f"ETA: {eta_hours} hours {eta_minutes} minutes")
     print(f"{progress:.2f}% ({m}/{n} files)")
     print(f"{'#' * num_hashes}{' ' * num_spaces}")
 
@@ -28,7 +48,7 @@ def progress_report():
 progress_report()
 
 # Schedule the progress_report function to run every 10 minutes
-schedule.every(10).minutes.do(progress_report)
+schedule.every(5).minutes.do(progress_report)
 
 # Keep the script running and execute the scheduled tasks
 while True:
